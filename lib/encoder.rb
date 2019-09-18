589b192a89512c808dcdc501917ac89fa9029c6a
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-17 20:27:47 -0600

    Add Encoder class with initialize, shift, and encode_message methods

---
+++ lib/encoder.rb
@@ -0,0 +1,43 @@
+require './lib/shift'
+
+class Encoder
+  def initialize(message, key, offset)
+    @message = message.downcase.split("")
+    @offset = offset.to_s.split("")
+    @key = key
+    @shift = shift
+    @letters = ("a".."z").to_a << " "
+    @encoded_message = []
+  end
+
+  def shift
+    @shift = Shift.new(@key, @offset).create_shift
+  end
+
+  def encode_message
+    @message.each_index do |i|
+      if !@letters.include?(@message[i])
+        @encoded_message.push(@message[i])
+      else
+        index = @letters.index(@message[i])
+        rotated = nil
+        if i.modulo(4) == 0
+          rotated = @letters.rotate(@shift["A"])
+        elsif i.modulo(4) == 1
+          rotated = @letters.rotate(@shift["B"])
+        elsif i.modulo(4) == 2
+          rotated = @letters.rotate(@shift["C"])
+        elsif i.modulo(4) == 3
+          rotated = @letters.rotate(@shift["D"])
+        end
+
+        if rotated == nil
+          @encoded_message << @message[i]
+        else
+          @encoded_message << rotated[index]
+        end
+      end
+    end
+    @encoded_message = @encoded_message.join
+  end
+end
