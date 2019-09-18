d2e2600ce8175650d7aa2ac6b0fcad1f79754798
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-17 20:27:59 -0600

    Add DecoderTest

---
+++ test/decoder_test.rb
@@ -0,0 +1,34 @@
+require './test/test_helper'
+require './lib/decoder'
+require './lib/shift'
+
+class DecoderTest < Minitest::Test
+  def setup
+    @key = "02715"
+    @offset = "1025"
+    @decoder = Decoder.new("keder ohulw", @key, @offset)
+    @shift = Shift.new(@key, @offset)
+  end
+
+  def test_it_exists
+    assert_instance_of Decoder, @decoder
+  end
+
+  def test_shift_returns_hash
+    actual = @decoder.shift
+    expected = {
+        "A" => 3,
+        "B" => 27,
+        "C" => 73,
+        "D" => 20
+    }
+    assert_equal expected, actual
+  end
+
+  def test_it_can_decode_message
+    actual = @decoder.decode_message
+    expected = "hello world"
+
+    assert_equal expected, actual
+  end
+end
